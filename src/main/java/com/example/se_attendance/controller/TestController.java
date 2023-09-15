4b3ba201b877bd0d16b00156f7b0194992008da5
JunhyeonKwak <jhkwak905@naver.com>
2023-09-15 23:10:25 +0900

    feat: add use jwt func

--- src/main/java/com/example/se_attendance/controller/TestController.java
+++ src/main/java/com/example/se_attendance/controller/TestController.java
@@ -3,6 +3,7 @@ package com.example.se_attendance.controller;
 
 import com.example.se_attendance.domain.dto.TestDTO;
 import com.example.se_attendance.service.TestService;
+import com.example.se_attendance.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -10,6 +11,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -43,5 +47,16 @@ public class TestController {
     @PostMapping("/test/jwt")
     public ResponseEntity<TestDTO.TestJwtResponse> returnJwt(@RequestBody TestDTO.TestJwtRequest dto) {
         return ResponseEntity.ok().body(testService.getJwt(dto));
+    }
+
+    // Jwt 사용해보기
+    @GetMapping("/")
+    public ResponseEntity<Map<String, String>> useJwt() {
+        String name = JwtUtil.getNameFromToken(); // 토큰에서 이름 가져오기
+
+        Map<String, String> response = new HashMap<>();
+        response.put("message", name + "님 수고하셨습니다.");
+
+        return ResponseEntity.ok().body(response);
     }
 }
