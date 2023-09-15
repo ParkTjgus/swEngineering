22b4895ae7017ff1c87ed892cb5590cd506d7d25
JunhyeonKwak <jhkwak905@naver.com>
2023-09-15 21:05:23 +0900

    Initial commit

---
+++ src/test/java/com/example/se_attendance/SeAttendanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.se_attendance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeAttendanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
