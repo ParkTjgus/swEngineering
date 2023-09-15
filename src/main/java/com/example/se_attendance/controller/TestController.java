0ffae1dc2c0d37906ee74be8ad2334aa8dde3dd1
JunhyeonKwak <jhkwak905@naver.com>
2023-09-15 21:30:46 +0900

    feat: add postTest

--- src/main/java/com/example/se_attendance/controller/TestController.java
+++ src/main/java/com/example/se_attendance/controller/TestController.java
@@ -1,18 +1,29 @@
 package com.example.se_attendance.controller;
 
 
+import com.example.se_attendance.domain.dto.TestDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 @RequiredArgsConstructor
 public class TestController {
 
+    // 아무것도 없이 Get 해보기
     @GetMapping("/")
     public ResponseEntity<String> getTest() {
         return ResponseEntity.ok().body("정상적으로 접근했습니다.");
+    }
+
+    // 메시지 Post 해보기
+    @PostMapping("/")
+    public ResponseEntity<String> postTest(@RequestBody TestDTO.TestRequest dto){
+        String message = dto.getMessage();
+
+        return ResponseEntity.ok().body(message);
     }
 }
---
+++ src/main/java/com/example/se_attendance/domain/dto/TestDTO.java
@@ -0,0 +1,11 @@
+package com.example.se_attendance.domain.dto;
+
+import lombok.Getter;
+
+public class TestDTO {
+
+    @Getter
+    public static class TestRequest {
+        private String message;
+    }
+}
