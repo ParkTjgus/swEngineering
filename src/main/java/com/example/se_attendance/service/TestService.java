d254d1d49c91c08464c1df36a716ae910d1607d9
JunhyeonKwak <jhkwak905@naver.com>
2023-09-15 22:29:06 +0900

    feat: add exceptionManager

--- src/main/java/com/example/se_attendance/service/TestService.java
+++ src/main/java/com/example/se_attendance/service/TestService.java
@@ -1,6 +1,8 @@
 package com.example.se_attendance.service;
 
 import com.example.se_attendance.domain.dto.TestDTO;
+import com.example.se_attendance.exeption.AppException;
+import com.example.se_attendance.exeption.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,9 +12,17 @@ public class TestService {
 
     // 쿼리 테스트 서비스
     public TestDTO.TestQueryResponse getTestQuery(TestDTO.TestQueryRequest dto) {
+        String name = dto.getName();
+        String message = dto.getMessage();
+
+        // null 또는 공백 문자열 검사
+        if (name == null || name.isBlank() || message == null || message.isBlank()) {
+            throw new AppException(ErrorCode.INVALID_INPUT,"모두 입력해 주세요!!");
+        }
+
         return TestDTO.TestQueryResponse.builder()
-                .name(dto.getName())
-                .message(dto.getMessage())
+                .name(name)
+                .message(message)
                 .build();
     }
 }
