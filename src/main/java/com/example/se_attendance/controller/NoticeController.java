8ecac36a9bb571bf66e871bc7ceb913a6362ab87
chair00 <101685159+chair00@users.noreply.github.com>
2023-11-14 17:31:13 +0900

    Update NoticeController.java

--- src/main/java/com/example/se_attendance/controller/NoticeController.java
+++ src/main/java/com/example/se_attendance/controller/NoticeController.java
@@ -15,11 +15,11 @@ import java.util.List;
 public class NoticeController {
     private final NoticeService noticeService;
 
-    //공지사항 생성
-    //생성한 공지사항의 id값이 리턴됨
+   //공지사항 생성
     @PostMapping("")
-    public void createNotice(@RequestBody NoticeDTO.NoticeDto noticeDto){
+    public ResponseEntity<String> createNotice(@RequestBody NoticeDTO.NoticeDto noticeDto){
         noticeService.createNotice(noticeDto);
+        return new ResponseEntity<>("공지사항이 등록되었습니다.", HttpStatus.OK);
     }
 
     //공지사항 조회
@@ -28,22 +28,24 @@ public class NoticeController {
         return noticeService.findNotice(id);
     }
 
-    //공지사항 목록 조회 추가!!
+    //공지사항 목록 조회
     @GetMapping("")
-    public List<NoticeDTO.NoticeDto> findAllNotice() { return noticeService.findAll(); }
+    public ResponseEntity<List<NoticeDTO.NoticeDto>> findAllNotice() {
+        return ResponseEntity.ok().body(noticeService.findAll());
+    }
 
 
-    //공지사항 수정
-    //생성시간, 수정시간도 뜨게 dto 변수 추가 후 수정하기?
-    @PatchMapping("/{id}")
-    public Long updateNotice(@PathVariable Long id, @RequestBody NoticeDTO.NoticeDto noticeDto){
-        return noticeService.updateNotice(id, noticeDto);
+    //가장 최근 공지사항 수정
+    @PatchMapping("")
+    public ResponseEntity<String> updateNotice(@RequestBody NoticeDTO.NoticeDto noticeDto){
+        noticeService.updateNotice(noticeDto);
+        return new ResponseEntity<>("공지사항이 수정되었습니다.", HttpStatus.OK);
     }
 
     //공지사항 삭제
     @DeleteMapping ("/{id}")
     public ResponseEntity<String> deleteNotice(@PathVariable Long id){
         noticeService.deleteNotice(id);
-        return new ResponseEntity<>("delete", HttpStatus.OK);
+        return new ResponseEntity<>("공지사항이 삭제되었습니다.", HttpStatus.OK);
     }
 }
