ebcc0c31d3c084e1591902a4cc75e791b6df8d91
chair00 <101685159+chair00@users.noreply.github.com>
2023-11-14 17:26:58 +0900

    Update MemberController.java
    App 회원 관리 코드 수정

--- src/main/java/com/example/se_attendance/controller/MemberController.java
+++ src/main/java/com/example/se_attendance/controller/MemberController.java
@@ -16,31 +16,34 @@ public class MemberController {
 
     private final MemberService memberService;
 
-    // 회원가입
+    // 회원가입 (app)
     @PostMapping("/signup")
-    public ResponseEntity<String> signUp( //Exception 안넣어주니까 오류 나던데 왜지? @NotNull과 관련?
+    public ResponseEntity<String> signUp(
                                           @RequestBody MemberDTO.MemberSignUpDto memberSignUpDto) throws Exception {
         memberService.signUp(memberSignUpDto);
         return new ResponseEntity<>("회원가입에 성공했습니다.", HttpStatus.OK);
     }
 
-    // 로그인
+    // 로그인 (app)
     @PostMapping("/login")
-    public ResponseEntity<String> login( //얘는 Exception 없어도 오류 안남? String으로 왜 작동함? service가 리턴하는게 string이라서?
+    public ResponseEntity<String> login(
                                          @RequestBody MemberDTO.MemberLoginDto memberLoginDto) {
-        return ResponseEntity.ok(memberService.login(memberLoginDto));
+        memberService.login(memberLoginDto);
+        return new ResponseEntity<>("로그인 되었습니다.", HttpStatus.OK);
     }
 
+    // 회원 정보 조회 (app)
     @GetMapping("/mypage")
     @ResponseBody
-    public MemberDTO.Memberdto getMyInfo() { //authentication이 뭐임? 이것만 인자로 넘겨줘도 알아서 찾을 수 있는거임?
+    public MemberDTO.Memberdto getMyInfo() {
         return memberService.findUser();
     }
 
+    // 회원 정보 수정 (app)
     @PatchMapping("/mypage")
-    public ResponseEntity<String> updateMyInfo(@RequestBody MemberDTO.Memberdto user){ //얘도 AuthenticationPrincipal 어노테이션 쓰면 되는걸까? 일단 entity를 매개변수로 넘겨주긴 해야함
+    public ResponseEntity<String> updateMyInfo(@RequestBody MemberDTO.Memberdto user){
         memberService.updateMyInfo(user);
-        return new ResponseEntity<>("회원 정보 수정 완", HttpStatus.OK);
+        return new ResponseEntity<>("회원 정보가 수정되었습니다.", HttpStatus.OK);
     }
 
     //DB에 저장된 회원 정보 전부 가져오기
