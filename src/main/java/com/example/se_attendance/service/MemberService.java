287b72d8944f1cdc44c84c77473bebcf9744055d
chair00 <101685159+chair00@users.noreply.github.com>
2023-11-14 17:29:12 +0900

    Update MemberService.java
    Exception 처리

--- src/main/java/com/example/se_attendance/service/MemberService.java
+++ src/main/java/com/example/se_attendance/service/MemberService.java
@@ -19,7 +19,7 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true) //롤백 기능 어노테이션. readonly쓰면 성능면에서 좋다네
+@Transactional(readOnly = true)
 @Slf4j
 public class MemberService {
     private final MemberRepository memberRepository;
@@ -27,10 +27,11 @@ public class MemberService {
     @Value("${jwt.secret}")
     private String secretKey;
 
+    // 회원가입
     @Transactional
     public void signUp(MemberDTO.MemberSignUpDto userDto) throws Exception {
         if (memberRepository.findByMemberId(userDto.getMemberId()).isPresent()) {
-            throw new Exception("이미 존재하는 아이디입니다.");
+            throw new AppException(ErrorCode.USERNAME_DUPLICATED, "이미 존재하는 아이디입니다.");
         }
 
         MemberEntity user = MemberEntity.builder()
@@ -45,17 +46,19 @@ public class MemberService {
         memberRepository.save(user);
     }
 
+    // 로그인
     public String login(MemberDTO.MemberLoginDto memberLoginDto) {
         Long expireTimeMs = 1000 * 60 * 60l;
 
         MemberEntity user = memberRepository.findByMemberId(memberLoginDto.getMemberId())
-                .orElseThrow(() -> new IllegalArgumentException("가입되지 않은 회원 입니다."));
+                .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND, "가입되지 않은 회원입니다."));
         if (!encoder.matches(memberLoginDto.getMemberPw(), user.getMemberPw())) {
-            throw new IllegalArgumentException("잘못된 비밀번호입니다.");
+            throw new AppException(ErrorCode.INVALID_INPUT, "잘못된 비밀번호입니다.");
         }
         return JwtUtil.createToken(user.getMemberId(), secretKey, expireTimeMs);
     }
 
+    // 회원 정보 조회
     public MemberDTO.Memberdto findUser() {
         String memberId = JwtUtil.getUserIdFromToken();
         MemberEntity user = memberRepository.findByMemberId(memberId).orElseThrow(() -> new BadCredentialsException("회원 정보를 찾을 수 없습니다."));
@@ -68,14 +71,17 @@ public class MemberService {
                 .build();
     }
 
+    // 회원 정보 수정
     @Transactional
     public void updateMyInfo(MemberDTO.Memberdto user) {
         MemberEntity persistance = memberRepository.findByMemberId(user.getMemberId()).orElseThrow(() -> new IllegalArgumentException("회원 정보를 찾을 수 없습니다."));
-        //persistance.setMemberPw(passwordEncoder.encode(user.getMemberPw()));
-        //persistance.setMemberName(user.getMemberName());
+        persistance.setMemberPw(encoder.encode(user.getMemberPw()));
+        persistance.setMemberName(user.getMemberName());
+        persistance.setMemberMajor(user.getMemberMajor());
+        persistance.setMemberState(user.getMemberState());
+        persistance.setMemberBirth(user.getMemberBirth());
         //더티체킹. 영속화된 persistance 객체의 변화가 감지되면 더티체킹이 되어 update문을 날려줌(db에)
     }
-
     public List<MemberDTO.MemberName> getAllMember(){
         List<MemberEntity> memberEntityList = memberRepository.findAll();
 
