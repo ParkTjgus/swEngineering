b85419fe4b6b0f07e4eb97c0a4c64ecc455afc0f
chair00 <101685159+chair00@users.noreply.github.com>
2023-11-21 00:56:52 +0900

    Update StudyGoalRepository.java
    findMembers (목표시간 달성한 멤버 출력하기) 추가

--- src/main/java/com/example/se_attendance/repository/StudyGoalRepository.java
+++ src/main/java/com/example/se_attendance/repository/StudyGoalRepository.java
@@ -10,6 +10,15 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public interface StudyGoalRepository extends JpaRepository<StudyGoalEntity, Long> {
+    // 가장 최근에 설정한 목표시간 가져오기
     @Query("SELECT e FROM StudyGoalEntity e WHERE e.createTime <= :targetDate ORDER BY e.createTime DESC")
     List<StudyGoalEntity> findLatestGoalsBeforeDate(LocalDateTime targetDate, Pageable pageable);
+
+    // 목표시간 달성한 멤버 출력하기
+    @Query("SELECT r.memberEntity.memberId, r.memberEntity.memberName, r.memberEntity.memberMajor, SUM(r.recordTime) " +
+            "FROM RecordEntity r " +
+            "WHERE FUNCTION('MONTH', r.createTime) = FUNCTION('MONTH', CURRENT_DATE) " +
+            "GROUP BY r.memberEntity.memberId, r.memberEntity.memberName, r.memberEntity.memberId " +
+            "HAVING SUM(r.recordTime) > :targetTime")
+    List<Object[]> findMembers(@Param("targetTime") Long targetTime);
 }
