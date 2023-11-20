d00cc58b190aa834424fc0ee4111886f75e440da
chair00 <101685159+chair00@users.noreply.github.com>
2023-11-21 00:59:01 +0900

    Update StudyGoalService.java
    findMembers (목표 시간 달성한 멤버 출력하기) 추가

--- src/main/java/com/example/se_attendance/service/StudyGoalService.java
+++ src/main/java/com/example/se_attendance/service/StudyGoalService.java
@@ -19,4 +19,32 @@ public class StudyGoalService {
                 .build();
         studyGoalRepository.save(studyGoal);
     }
+
+    // 목표시간 달성한 멤버 출력하기 (web)
+    public List<MemberDTO.rankMember> findMembers() {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM");
+        String month = now.format(formatter);
+
+        int targetTimei = recordService.getStudyGoal(month).getStudyGoal();
+        Long targetTime = Long.valueOf(targetTimei);
+
+        List<Object[]> result = studyGoalRepository.findMembers(targetTime);
+
+
+        List<MemberDTO.rankMember> mappedResult = new ArrayList<>();
+        for (Object[] row : result) {
+
+            String memberId = (String) row[0];
+            String memberName = (String) row[1];
+            String memberMajor = (String) row[2];
+            int recordTime = ((Number) row[3]).intValue();
+
+            MemberDTO.rankMember member = new MemberDTO.rankMember(memberId, memberName, memberMajor, recordTime);
+            mappedResult.add(member);
+        }
+
+        return mappedResult;
+
+    }
 }
