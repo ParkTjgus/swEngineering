4b4f1c521204c5e1e0c33b672549d8a4a7c702ef
chair00 <101685159+chair00@users.noreply.github.com>
2023-11-21 01:13:17 +0900

    Update NoticeController.java
    공지사항 목록 조회 (web) 수정

--- src/main/java/com/example/se_attendance/controller/NoticeController.java
+++ src/main/java/com/example/se_attendance/controller/NoticeController.java
@@ -15,26 +15,26 @@ import java.util.List;
 public class NoticeController {
     private final NoticeService noticeService;
 
-   //공지사항 생성
+   //공지사항 생성 (web)
     @PostMapping("")
     public ResponseEntity<String> createNotice(@RequestBody NoticeDTO.NoticeDto noticeDto){
         noticeService.createNotice(noticeDto);
         return new ResponseEntity<>("공지사항이 등록되었습니다.", HttpStatus.OK);
     }
 
-    //공지사항 조회
+    //공지사항 조회 (web)
     @GetMapping("/{id}")
     public NoticeDTO.NoticeDto findNotice(@PathVariable Long id){
         return noticeService.findNotice(id);
     }
 
-    //공지사항 목록 조회
+    //공지사항 목록 조회 (web)
     @GetMapping("")
-    public ResponseEntity<List<NoticeDTO.NoticeDto>> findAllNotice() {
+    public ResponseEntity<List<NoticeEntity>> findAllNotice() {
         return ResponseEntity.ok().body(noticeService.findAll());
     }
 
-    //공지사항 삭제
+    //공지사항 삭제 (web)
     @DeleteMapping ("/{id}")
     public ResponseEntity<String> deleteNotice(@PathVariable Long id){
         noticeService.deleteNotice(id);
